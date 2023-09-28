7db96d49f60265033e34671090e872884e3ca0d2
Nick Jones <tidalboot@googlemail.com>
2023-09-28 11:08:54 +0100

    initial project commit

---
+++ HowsItLooking/HowsItLooking/HowsItLookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowsItLookingApp.swift
+//  HowsItLooking
+//
+//  Created by Nick Jones on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HowsItLookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
